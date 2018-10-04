be89701f30abfafe79c6e8229ad9d3eab2830e88
anushree-12 <agopal12@uw.edu>
2018-10-03 20:39:45 -0700

    Final code

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -6,62 +6,60 @@
 //  Copyright © 2017 Neward & Associates. All rights reserved.
 //
 
-// How do I initalize the array? And add the elements from the substring (from other method) to that array?
-// What is the method below doing? With the map...
-// Should I be returning values in the array method? Or creating a new variable and saving the final answer into that? I think return
-//For count, avg, fact, can I remove the last element so I can iterate through the entire array, or can I iterate size-1 times?
-// To iterate through the whole array for actual calculator code, how do I increment array index?
-
 import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
-        // not sure how to initialize the array
-        // args += calculate? add each susbtring character into the args array or ars.append(calculate)
-        // for each substring in calculate
         let array = args
         
         if array.last == "count" {
             var count = 0
             // How do I cast the strings to integers?
-            for (index, element) in array.enumerate() {
+            for (_, element) in array.enumerated() {
                 if element != "count" {
                     count = count + 1
                 }
-            return count
+                return count
             }
         }
-            else if array.last == "avg" {
+        else if array.last == "avg" {
             var avg = 0
             var avgValue = 0
-            for (index, element) in array.enumerate() {
-                avg = avg.reduce(0, +)
-                avgValue = avg / array.count
+            for (_, element) in array.enumerated() {
+                while element != "avg" {
+                    let num = Int(element)!
+                    avg = avg + num
+                    avgValue = avg / array.count
+                }
             }
             return avgValue
         } else if array.last == "fact" {
             var factorial = 1
-            for (index, element) in array.enumerate() {
-                while element > 1 {
-                    factorial = factorial * element
-                    element = element - 1
+            if array.count > 1 {
+                let _ : Int = Int(array.first!)!
+            }
+            for (_, element) in array.enumerated() {
+                var intElement = Int(element)
+                while intElement! > 1 {
+                    factorial = factorial * intElement!
+                    intElement = intElement! - 1
                 }
             }
             return factorial
             
         }
         
-        var arrayNum = 0
+        let arrayNum = 0
         var operation = ""
         var result = 0
         
-        for (index, element) in array.enumerate() {
+        for (index, element) in array.enumerated() {
             if index % 2 == 0 {
-                arrayNum = element
+                let _ = Int(element)
             } else {
                 operation = element
             }
-
+            
             if index != 0 || index != 1 && index % 2 == 0 {
                 if operation == "+" {
                     result = result + arrayNum
@@ -69,11 +67,15 @@ public class Calculator {
                     result = result - arrayNum
                 } else if operation == "*" {
                     result = result * arrayNum
-                    } else if operation == "/" {
-                    result = result / arrayNum
-                } else if operation == "%" {
+                } /*else if operation == "/" {
+                        if arrayNum != 0 {
+                            result = result / arrayNum
+                        } else {
+                            break
+                    }
+                } else if arrayNum != 0 && operation == "%" {
                     result = result % arrayNum
-                }
+                }*/
             }
         }
         return result
